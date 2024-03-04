5c585197f69ebe509e01d7e756e69519bd1706ba
Daniel Duarte <d.ignacio292@gmail.com>
2024-03-04 12:37:10 -0300

    Update CORS CRUD

--- src/main/java/com/api/crud/CrudApplication.java
+++ src/main/java/com/api/crud/CrudApplication.java
@@ -2,6 +2,9 @@ package com.api.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,4 +13,15 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/user/**") // aquí especificas el endpoint que deseas permitir CORS
+						.allowedOrigins("http://localhost:5173") // permites solicitudes solo desde este origen del front
+						.allowedMethods("GET", "POST", "PUT", "DELETE"); // permites estos métodos HTTP
+			}
+		};
+	}
 }
